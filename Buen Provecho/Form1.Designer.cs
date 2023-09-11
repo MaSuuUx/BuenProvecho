71fa58f546e9d9d4680b25c4c52096b06ca2011b
Marcelo Monge <111376835+MaSuuUx@users.noreply.github.com>
2023-09-10 22:14:51 -0600

    Add files via upload

---
+++ Buen Provecho/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Buen_Provecho
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.BtnPrint = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(185, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(548, 48);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Sistema de Facturacion";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(21, 297);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(262, 50);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ingrese el consumo\ndel cliente:                $";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(312, 320);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(249, 27);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // BtnPrint
+            // 
+            this.BtnPrint.BackColor = System.Drawing.Color.ForestGreen;
+            this.BtnPrint.Font = new System.Drawing.Font("Verdana", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnPrint.ForeColor = System.Drawing.Color.White;
+            this.BtnPrint.Location = new System.Drawing.Point(591, 276);
+            this.BtnPrint.Name = "BtnPrint";
+            this.BtnPrint.Size = new System.Drawing.Size(130, 44);
+            this.BtnPrint.TabIndex = 4;
+            this.BtnPrint.Text = "Imprimir";
+            this.BtnPrint.UseVisualStyleBackColor = false;
+            this.BtnPrint.Click += new System.EventHandler(this.BtnPrint_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(156, 212);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Font = new System.Drawing.Font("Verdana", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(613, 550);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 50);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Cerrar";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // errorProvider
+            // 
+            this.errorProvider.ContainerControl = this;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button1.Font = new System.Drawing.Font("Verdana", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(591, 337);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 44);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Limpiar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(-1, 597);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(315, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Hector Marcelo Monge Caballero-MC23084";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cornsilk;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(748, 626);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.BtnPrint);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Facturas Buen Provecho";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button BtnPrint;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ErrorProvider errorProvider;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Buen Provecho/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+ -------------------------------------------------------------------------------------------
+ ===> NOMBRE: HECTOR MARCELO MONGE CABALLERO
+ ===> CARNET: MC23084
+ ===> PROFESOR: ING. BLADIMIR DIAZ
+ ===> GL: 18
+
+ENUNCIADO: La Cadena de Restaurantes “Buen Provecho” desea un programa en el cual:
+
+a) Si el cliente ha consumido más de $30.00, se le aplique un descuento del 25% a su
+cuenta y se muestre en pantalla lo total a cobrar y su descuento.
+
+b) Si el cliente gastó $30.00 o menos, no se le aplique el descuento y pague la totalidad
+de su consumo. Desplegar el mensaje: “Pasar a Caja por un Cupón Promocional”.
+
+ -------------------------------------------------------------------------------------------
+ */
+namespace Buen_Provecho
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //EVENTO PARA CERRAR EL FORMS
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        //EVENTO PARA DESHABILITAR EL BOTON AL CARGAR EL FORM
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BtnPrint.Enabled = false;
+        }
+
+        //EVENTO DE TXTCHANGED
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            ctrlBotones();//LLAMADO AL METODO
+        }
+
+        private void ctrlBotones()
+        {
+            string entradaData = textBox1.Text;
+
+            // VERIFICA SI EL TEXTO NO ESTÁ EN BLANCO Y ES UN NÚMERO ENTERO O DECIMAL
+            if (!string.IsNullOrWhiteSpace(entradaData) && (EsNumeroEntero(entradaData) || EsNumeroDecimal(entradaData)))
+            {
+                // HABILITA EL BOTON
+                BtnPrint.Enabled = true;
+                errorProvider.Clear();
+            }
+            else
+            {
+                // DESHABILITA EL BOTON
+                BtnPrint.Enabled = false;
+
+                // MUESTRA UN MENSAJE DE ERROR 
+                errorProvider.SetError(textBox1, "Debe ingresar solo números (enteros o decimales)");
+            }
+        }
+
+        //VERIFICA SI ES ENTERO
+        private static bool EsNumeroEntero(string entradaData)
+        {
+            int number;
+            return int.TryParse(entradaData, out number);
+        }
+
+        //VERIFICA SI ES DECIMAL
+        private static bool EsNumeroDecimal(string entradaData)
+        {
+            decimal number;
+            return decimal.TryParse(entradaData, out number);
+        }
+
+        //EVENTO DEL BtnPrint_Click
+        private void BtnPrint_Click(object sender, EventArgs e)
+        {
+            string entradaData = textBox1.Text;
+
+            if (double.TryParse(entradaData, out double montoConsumido))
+            {
+                string mensaje = "";
+
+                //SWICTH DEPENDIENDO DEL CASO
+                switch (montoConsumido)
+                {
+                    case double monto when (monto > 30.00):
+                        double descuento = monto * 0.25;
+                        double totalAPagar = monto - descuento;
+                        mensaje = $"Total a pagar: ${totalAPagar:F2}\nDescuento aplicado: ${descuento:F2}";
+                        break;
+
+                    case double monto when (monto <= 30.00 && monto > 0):
+                        mensaje = $"Total a pagar: ${monto:F2}\nPasar a Caja por un Cupon Promocional ";
+                        break;
+
+                    default:
+                        mensaje = "Monto no valido";
+                        break;
+                }
+
+                //CONFIGURACION DEL MBOX
+                MessageBox.Show(mensaje, "Resultado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        //EVENTO PARA LIMPIAR EL TXTBOX
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+    }
+}
